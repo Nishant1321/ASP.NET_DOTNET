dc6e7bd2c75ea34c4fd79e23f2dfdb56a9817098
Nishant Ingawale <97471023+Nishant1321@users.noreply.github.com>
2024-04-15 22:51:41 +0530

    Add files via upload

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace LIST.Pages
+namespace CRUDEAPP_1.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
---
+++ Pages/Users/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using CRUDEAPP_1.Data;
+using CRUDEAPP_1.Model;
+
+namespace CRUDEAPP_1.Pages.Users
+{
+    public class CreateModel : PageModel
+    {
+        private readonly CRUDEAPP_1.Data.CRUDEAPP_1Context _context;
+
+        public CreateModel(CRUDEAPP_1.Data.CRUDEAPP_1Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public User User { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.User.Add(User);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
